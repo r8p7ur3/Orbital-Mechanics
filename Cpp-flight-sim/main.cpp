4497b710f3050ab321da2c51e26467d34ec65314
jonathan namanda <50199159+r8p7ur3@users.noreply.github.com>
2026-09-03 16:46:50 +0100

    fixed libraries for openGL

--- Cpp-flight-sim/main.cpp
+++ Cpp-flight-sim/main.cpp
@@ -1,25 +1,57 @@
 #define FPL_IMPLEMENTATION
 #include "final_platform_layer.h"
-
+#include <math.h>
+#include <glbinding/gl/gl.h>
 #include <iostream>
 using namespace std;
+using namespace gl;
 // soul class (using for objects that move and interact with eachother later)
 class soul {
-    public:
+    private:
     double speed;
     double acceleration;
 
+
 };
 
+class values {
+    private:
+    string windowtitle;
+
+};
 char mexico ='P';
 
 const char* title = &mexico;
 
+//testing something
+fplWindowSize curSize;
 
 int main (int argc, char **argv) {
-    fplSetWindowTitle (title);
+
+ 
+fplSettings settings;
+fplSetDefaultSettings(&settings);
+fplVideoSettings &videoSettings = settings.video;
+
+
+//Open Gl for video backend
+videoSettings.backend = fplVideoBackendType_OpenGL;
+
 // initialize window with video context
-    if (fplPlatformInit(fplInitFlags_Window | fplInitFlags_Video, fpl_null)) {
+    if (fplPlatformInit(fplInitFlags_Window | fplInitFlags_Video, &settings)) {
+
+        glClearColor(0.0,0.0,0.0,1.0);
+        //setting window string (Chikun cover ist sehr gut )
+        fplSetWindowTitle("MIT ALLER MACHT");
+        //all main OpenGL commands here
+        if (fplGetWindowSize(&curSize)){
+        
+
+        }
+        // set window size to 800x600
+        fplSetWindowSize(800,600);
+        char titleBuffer[FPL_MAX_NAME_LENGTH];
+        fplGetWindowTitle(titleBuffer, fplArrayCount(titleBuffer));
 
 
         
@@ -30,6 +62,7 @@ int main (int argc, char **argv) {
 
             }
         }
+
 
 //release window
         fplPlatformRelease();
