75a9fa3369483cf38aa265e74554555386d2bd40
jonathan namanda <50199159+r8p7ur3@users.noreply.github.com>
2026-09-03 02:21:57 +0100

    Add files via upload

--- Cpp-flight-sim/main.cpp
+++ Cpp-flight-sim/main.cpp
@@ -3,17 +3,26 @@
 
 #include <iostream>
 using namespace std;
-
+// soul class (using for objects that move and interact with eachother later)
 class soul {
     public:
     double speed;
     double acceleration;
 
 };
+
+char mexico ='P';
+
+const char* title = &mexico;
+
+
 int main (int argc, char **argv) {
+    fplSetWindowTitle (title);
 // initialize window with video context
     if (fplPlatformInit(fplInitFlags_Window | fplInitFlags_Video, fpl_null)) {
 
+
+        
 //loop that keeps communication between running window and application (i think)
         while(fplWindowUpdate()) {
             fplEvent ev;
@@ -22,6 +31,7 @@ int main (int argc, char **argv) {
             }
         }
 
+//release window
         fplPlatformRelease();
         return 0;
     } else {
